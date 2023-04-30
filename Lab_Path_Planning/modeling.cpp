a3e33c75d18e43f51b68795ccf8b0e3a647f6aa4
bmaxdk <choh8uw@gmail.com>
2023-04-30 15:45:11 -0400

    Update modeling

--- Lab_Path_Planning/modeling.cpp
+++ Lab_Path_Planning/modeling.cpp
@@ -18,7 +18,7 @@ class Map{
 public:
     const static int w = 6;
     const static int h = 5;
-    vector<vector<int> grid = {
+    vector<vector<int>> grid = {
                                 {0, 1, 0, 0, 0, 0},
                                 {0, 1, 0, 0, 0, 0},
                                 {0, 1, 0, 0, 0, 0},
@@ -36,7 +36,7 @@ public:
     int start[2] = {0, 0};
     int goal[2] = {h-1, w-1};
     int cost = 1;
-    string movement_arrows[4] = {"^", "<", "v", ">"};
+    string movements_arrows[4] = {"^", "<", "v", ">"};
     int movements[4][2] = {{-1, 0}, {0, -1}, {1,0}, {0,1}};
 };
 
@@ -57,6 +57,33 @@ public:
     c d
     use templates
 */
+template <typename T>
+void print2DVector(T &Vec)
+{
+    for (int i=0; i<sizeof(Vec)/sizeof(Vec[0]); i++)
+    {
+        for(int j=0; j<sizeof(Vec[0])/sizeof(Vec[0][0]); j++)
+        {
+
+            cout << Vec[i][j] << ' ';
+        }
+        cout<<endl;
+    }
+}
+
+// Specialization for dynamic 2D vectors
+template <typename T>
+void print2DVector(vector<vector<T>> Vec)
+{
+    for (int i = 0; i < Vec.size(); i++)
+    {
+        for (int j = 0; j < Vec[i].size(); j++)
+        {
+            cout << Vec[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
 
 /*############ Don't modify the main function############*/
 int main()
@@ -77,3 +104,74 @@ int main()
 
     return 0;
 }
+
+
+
+// #include <iostream>
+// #include <string.h>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// // Map class
+// class Map {
+// public:
+//     const static int mapWidth = 6;
+//     const static int mapHeight = 5;
+//     vector<vector<int> > grid = {
+//         { 0, 1, 0, 0, 0, 0 },
+//         { 0, 1, 0, 0, 0, 0 },
+//         { 0, 1, 0, 0, 0, 0 },
+//         { 0, 1, 0, 0, 0, 0 },
+//         { 0, 0, 0, 1, 1, 0 }
+//     };
+// };
+
+// // Planner class
+// class Planner : Map {
+// public:
+//     int start[2] = { 0, 0 };
+//     int goal[2] = { mapHeight - 1, mapWidth - 1 };
+//     int cost = 1;
+
+//     string movements_arrows[4] = { "^", "<", "v", ">" };
+
+//     vector<vector<int> > movements{
+//         { -1, 0 },
+//         { 0, -1 },
+//         { 1, 0 },
+//         { 0, 1 }
+//     };
+// };
+
+// // Template function to print 2D vectors of any type
+// template <typename T>
+// void print2DVector(T Vec)
+// {
+//     for (int i = 0; i < Vec.size(); ++i) {
+//         for (int j = 0; j < Vec[0].size(); ++j) {
+//             cout << Vec[i][j] << ' ';
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main()
+// {
+//     // Instantiate map and planner objects
+//     Map map;
+//     Planner planner;
+
+//     // Print classes variables
+//     cout << "Map:" << endl;
+//     print2DVector(map.grid);
+//     cout << "Start: " << planner.start[0] << " , " << planner.start[1] << endl;
+//     cout << "Goal: " << planner.goal[0] << " , " << planner.goal[1] << endl;
+//     cout << "Cost: " << planner.cost << endl;
+//     cout << "Robot Movements: " << planner.movements_arrows[0] << " , " << planner.movements_arrows[1] << " , " << planner.movements_arrows[2] << " , " << planner.movements_arrows[3] << endl;
+//     cout << "Delta:" << endl;
+//     print2DVector(planner.movements);
+
+//     return 0;
+// }
