c4dfec44a749a4010c3b40d4ece95aa9473be4f5
bmaxdk <choh8uw@gmail.com>
2023-05-03 00:19:22 -0400

    Update BFS Shortest Path

--- Lab_Path_Planning/4_BFS_Shortest_Path3.cpp
+++ Lab_Path_Planning/4_BFS_Shortest_Path3.cpp
@@ -61,6 +61,8 @@ void search(Map map, Planner planner)
     map.grid[r][c] =1;
     store.push_back({g,r,c});
     vector<vector<int> > expand(map.mapHeight, vector<int>(map.mapWidth, -1));
+    vector<vector<int> > path(map.mapHeight, vector<int>(map.mapWidth, -1));
+
     int cost = 0;
     expand[0][0] = 0;
     while(!store.empty())
@@ -85,42 +87,26 @@ void search(Map map, Planner planner)
                 store.push_back({curr_g+planner.cost, newr, newc});
                 map.grid[newr][newc] = 1;
                 expand[newr][newc] = ++cost;
+                path[newr][newc] = i;
             }
         }
     }
 
-    print2DVector(expand);
-    vector<vector<string>> path(map.mapHeight, vector<string>(map.mapWidth, "-"));
-    int cur_r, cur_c, cur_g;
-    cur_r = planner.start[0];
-    cur_c = planner.start[1];
-    cur_g = expand[cur_r][cur_c];
-    while(cur_r != planner.goal[0] or cur_c != planner.goal[1])
-    {
-        int pos = -1;
-        string assign;
-        for(int i=0; i<planner.movements.size(); i++)
-        {
-            int new_r = cur_r + planner.movements[i][0];
-            int new_c = cur_c + planner.movements[i][1];
-            if (0<= new_r and new_r<expand.size() and 0<=new_c and new_c<expand[0].size())
-            {
-                int new_g = expand[new_r][new_c];
-                if (cur_g < new_g)
-                {
-                    cur_g = new_g;
-                    pos = i;
-                }
-            }
-        }
-        assign = planner.movements_arrows[pos];
-        path[cur_r][cur_c] = assign;
-        cur_r +=  planner.movements[pos][0];
-        cur_c +=  planner.movements[pos][1];
-     }
-    path[planner.goal[0]][planner.goal[1]] = "*";
     print2DVector(path);
-
+    vector<vector<string>> direction(map.mapHeight, vector<string>(map.mapWidth, "-"));
+    int cur_r, cur_c;
+    cur_r = planner.goal[0];
+    cur_c = planner.goal[1];
+    direction[cur_r][cur_c] = "*";
+    while (cur_r!=planner.start[0] or cur_c!=planner.start[1])
+    {
+        int i = path[cur_r][cur_c];
+        cur_r -= planner.movements[i][0];
+        cur_c -= planner.movements[i][1];
+        direction[cur_r][cur_c] = planner.movements_arrows[i];
+    }
+    cout<<endl;
+    print2DVector(direction);
 }
 
 int main()
@@ -135,9 +121,4 @@ int main()
 
     return 0;
 }
-
-// Output
-// [11, 4, 5]
-
-
 
