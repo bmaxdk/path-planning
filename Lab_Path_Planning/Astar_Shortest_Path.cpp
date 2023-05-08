b79a1c715257965be4950163a12e356e64c9bf51
bmaxdk <choh8uw@gmail.com>
2023-05-08 19:51:19 -0400

    Upload A*

--- Lab_Path_Planning/Astar_Shortest_Path.cpp
+++ Lab_Path_Planning/Astar_Shortest_Path.cpp
@@ -5,17 +5,24 @@
 
 using namespace std;
 
-// TODO: Add a Manhattan-based heuristic vector to the Map class
+// *Add a Manhattan-based heuristic vector to the Map class
 class Map {
 public:
     const static int mapWidth = 6;
     const static int mapHeight = 5;
-    vector<vector<int> > grid = {
+    vector<vector<int>> grid = {
         { 0, 1, 0, 0, 0, 0 },
         { 0, 1, 0, 0, 0, 0 },
         { 0, 1, 0, 0, 0, 0 },
         { 0, 1, 0, 0, 0, 0 },
         { 0, 0, 0, 1, 1, 0 }
+    };
+    vector<vector<int>> heuristic = {
+        { 9, 8, 7, 6, 5, 4 },
+        { 8, 7, 6, 5, 4, 3 },
+        { 7, 6, 5, 4, 3, 2 },
+        { 6, 5, 4, 3, 2, 1 },
+        { 5, 4, 3, 2, 1, 0 }
     };
 };
 
@@ -48,7 +55,7 @@ void print2DVector(T Vec)
     }
 }
 
-/* #### TODO: Modify the search function and implement the A* algorithm #### */
+/* *Modify the search function and implement the A* algorithm #### */
 void search(Map map, Planner planner)
 {
     // Create a closed 2 array filled with 0s and first element 1
@@ -65,10 +72,11 @@ void search(Map map, Planner planner)
     int x = planner.start[0];
     int y = planner.start[1];
     int g = 0;
+    int f = f+map.heuristic[x][y];
 
     // Store the expansions
     vector<vector<int> > open;
-    open.push_back({ g, x, y });
+    open.push_back({f, g, x, y });
 
     // Flags and counters
     bool found = false;
@@ -95,9 +103,9 @@ void search(Map map, Planner planner)
             next = open.back();
             open.pop_back();
 
-            x = next[1];
-            y = next[2];
-            g = next[0];
+            x = next[2];
+            y = next[3];
+            g = next[1];
 
             // Fill the expand vectors with count
             expand[x][y] = count;
@@ -117,7 +125,8 @@ void search(Map map, Planner planner)
                     if (x2 >= 0 && x2 < map.grid.size() && y2 >= 0 && y2 < map.grid[0].size()) {
                         if (closed[x2][y2] == 0 and map.grid[x2][y2] == 0) {
                             int g2 = g + planner.cost;
-                            open.push_back({ g2, x2, y2 });
+                            f = g2 + map.heuristic[x2][y2];
+                            open.push_back({f, g2, x2, y2 });
                             closed[x2][y2] = 1;
                             action[x2][y2] = i;
                         }
@@ -127,7 +136,7 @@ void search(Map map, Planner planner)
         }
     }
     // Print the expansion List
-    //print2DVector(expand);
+    print2DVector(expand);
 
     // Find the path with robot orientation
     vector<vector<string> > policy(map.mapHeight, vector<string>(map.mapWidth, "-"));
@@ -144,7 +153,7 @@ void search(Map map, Planner planner)
         x = x2;
         y = y2;
     }
-
+    cout << endl;
     // Print the path with arrows
     print2DVector(policy);
 }
